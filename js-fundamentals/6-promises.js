35b7b21022350aa2a1ca8f25b11d6b18fa55acd6
Aaron Carrasco <vaaroncarrasco@gmail.com>
2022-04-25 03:34:50 -0500

    updated promises class content

--- js-fundamentals/6-promises.js
+++ js-fundamentals/6-promises.js
@@ -1,10 +1,22 @@
 // ? Promises
 /*
 
-JS is an async, single threaded language. This is possible thanks
-to the Event Loop, which works with promises.
+JavaScript is an asynchronous programming language that runs on a single thread. This means
+that a slow and time consuming process that is not processed asynchronously can block the
+execution of the rest of our code.
 
-A promise is an object returned by an async task.
+To solve this, JavaScript introduces the event loop, or event cycle. The event loop is made
+up of two main components, a message queue and a loop that iterates through this message queue.
+Asynchronous programming in JavaScript works by pushing certain operations to this activity queue,
+so that they don't block code execution while they finish, the job of the event loop is to ask the
+activity queue operations if they have finished, and when they do. , resumes execution of that operation.
+
+In order to work with the event loop, JavaScript introduces the concept of promises, which allow
+us to define code to be executed when a task is finished.
+
+A promise is a type of object returned by an asynchronous operation, using this object we can obtain
+the final value of the asynchronous operation, or the possible errors found in its execution. This is
+why we commonly say that a promise is an object that produces a value in the future.
 
 * The promise can be at 4 different states:
     1. Pending: Async task hasnt finished yet.
